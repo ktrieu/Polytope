475ad2159ca876af00f5a8ba0c4ceb3c886244fe
Kevin Trieu <dragn194@gmail.com>
2019-10-05 17:08:25 -0400

    Rotate entities on load
    Blender's coordinate systems and the OpenGL default don't match. Eventually, we'll rotate in the tools project instead.

--- Polytope/src/world/Entity.cpp
+++ Polytope/src/world/Entity.cpp
@@ -4,6 +4,9 @@
 
 Entity::Entity(Mesh& mesh, glm::vec3 pos = glm::vec3(0.0f, 0.0f, 0.0f)) : m_mesh(mesh) {
 	m_transform = glm::translate(m_transform, pos);
+	//blender exported meshes have to be rotated to fit OpenGL's coord system. Eventually, we'll do this
+	//on the tools side
+	m_transform = glm::rotate(m_transform, -90.0f, glm::vec3(1.0f, 0.0f, 0.0f));
 }
 
 Entity::~Entity() {
